b58a38798edf548452233fba95589f8ce7087ddf
Aman Kumar Singh <amans.ug21.ec@nitp.ac.in>
2025-03-08 12:01:59 +0530

    Redux Toolkit project started and store and reducers created

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Redux Toolkit Todo Project</h1>
+    </div>
+  )
+}
+
+export default App
